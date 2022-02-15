b33a6f46cd24e873602e9217d05900f6684baddb
Korey Andrus <korey86@gmail.com>
2022-02-15 10:59:10 -0600

    complete class 1 exercises

---
+++ C1_Getting-Started_JavaScript/C1_Exercises/Ex2/main.js
@@ -0,0 +1,6 @@
+function changeHeading() {
+    const myHeading = document.querySelector("h1");
+
+    myHeading.textContent = "Korey Andrus - Web Developer";
+    myHeading.style.boxShadow = "rgba(0, 0, 0, 0.25) 0px 25px 50px -12px";
+}
