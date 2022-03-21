06ad40bd98659b57d544def6f3109c5bc5cd3f48
WilderDev <williamjohnwilder@outlook.com>
2022-03-21 04:27:57 -0500

    REMOVE: Class Notes

--- C10__Introduction-to-Typescript__Angular/Class-Code/main.ts
+++
@@ -1,9 +0,0 @@
-const addNumbers = (a: number, b: number) => a + b;
-
-const sumWithNumbers = addNumbers(4, 3);
-// const sumWithMixed = addNumbers(4, "3");
-// const sumWithStrings = addNumbers("4", "3");
-
-console.log("SUM w/ Numbers:", sumWithNumbers, "~>", typeof sumWithNumbers);
-// console.log("SUM w/ Mixed:", sumWithMixed, "~>", typeof sumWithMixed);
-// console.log("SUM w/ Strings:", sumWithStrings, "~>", typeof sumWithStrings);
--- C10__Introduction-to-Typescript__Angular/Class/C10_EXERCISES.md
+++ C10__Introduction-to-Typescript__Angular/Class/C10_EXERCISES.md
@@ -1,6 +1,5 @@
 # Introduction to Typescript Exercises
 
-
 - [Typescript Exercises 1- 4: Pair Programming](https://typescript-exercises.github.io/#exercise=1&file=%2Findex.ts)
 
 - Refactor all exercise solutions to use Typescript
